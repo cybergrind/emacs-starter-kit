586c8d7c03bfdc1bbf68541d6898b4f04f9118dc
kpi <kpi@cybergrind.(none)>
2010-06-02 18:07:28 +0300

    init

---
+++ cybergrind.el
@@ -0,0 +1,17 @@
+;(custom-set-variables
+;  ;; custom-set-variables was added by Custom.
+;  ;; If you edit it by hand, you could mess it up, so be careful.
+;  ;; Your init file should contain only one such instance.
+;  ;; If there is more than one, they won't work right.
+; )
+;(custom-set-faces
+;  ;; custom-set-faces was added by Custom.
+;  ;; If you edit it by hand, you could mess it up, so be careful.
+;  ;; Your init file should contain only one such instance.
+;  ;; If there is more than one, they won't work right.
+; '(org-level-4 ((t (:inherit outline-4 :foreground "#F04" :weight bold)))))
+;
+;(setq inferior-lisp-program "/usr/bin/clisp")
+;(add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
+;(require 'slime)
+;(slime-setup)
