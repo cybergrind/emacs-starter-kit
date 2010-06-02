8987531a98ffab1a0e9dbf53c2e6319d9408c7c1
kpi <kpi@cybergrind.kpi>
2010-06-02 18:12:46 +0300

    lisp fix

--- cybergrind.el
+++ cybergrind.el
@@ -15,3 +15,10 @@
 ;(add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
 ;(require 'slime)
 ;(slime-setup)
+
+
+(setq inferior-lisp-program "/usr/bin/clisp")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
+(require 'slime)
+(slime-setup)
+
